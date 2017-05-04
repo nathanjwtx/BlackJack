ded90932964a96a05d0767756c7f9083a59bf6ac
Nathan <dynamonath@nathanjwtx.me>
2017-05-03 19:30:16 -0500

    Empty/reset database query for testing

--- Testing/reset_db.py
+++ Testing/reset_db.py
@@ -4,5 +4,8 @@ db = sql.connect('C:/BlackJack/game_save.sqlite')
 cursor = db.cursor()
 
 cursor.execute('''
-    DELETE from players WHERE player_id = 21
+    DELETE FROM players
 ''')
+
+db.commit()
+db.close()
